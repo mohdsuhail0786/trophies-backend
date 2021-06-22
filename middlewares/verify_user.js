dd72635b2b864b66e47023d6c3e9b0d3c5946c4a
mohdsuhail0786 <mohdsuhail7khan0786@gmail.com>
2021-06-22 10:45:26 +0530

    forgot password working now

--- middlewares/verify_user.js
+++ middlewares/verify_user.js
@@ -3,25 +3,26 @@ const globalConstant=require('../utils/globalConstant');
 const apiUtils=require('../utils/apiUtils');
 const logger = require('../utils/logger');
 const HttpStatus=require('http-status-codes');
+const User=require('../models/userSchema')
 
 const verifyUser=(req,res,next)=>{
     logger.debug('inside verifyUser middleware');
-    let token;
-    if(req.params.token){
-        token=req.params.token;
-    }
-    else{
-        token=req.headers[globalConstant.TOKEN];;
-    }
+    const token=req.headers[globalConstant.TOKEN];
     if(token){
+        logger.debug('token detected');
         apiUtils.verifyAccessToken(token,process.env.TOKEN_SECRET)
         .then((user)=>{
-            req.user=user;
+            logger.debug('token verified')
+            return User.findById(user.id)
+        })
+        .then((originalUser)=>{
+            logger.debug('user found in db')
+            req.user=originalUser;
             next();
         })
         .catch((err)=>{
-            logger.debug('token is unauthentic');
-            res.status(400).json(apiUtils.getResponseMessage(400,'token is unauthentic'));
+            logger.debug('token is unauthentic :: '+err);
+            res.status(HttpStatus.BAD_REQUEST).json(apiUtils.getResponseMessage(HttpStatus.BAD_REQUEST,'token is unauthentic'));
         })
     }
     else{
@@ -30,4 +31,6 @@ const verifyUser=(req,res,next)=>{
     }
 }
 
+
 module.exports=verifyUser;
+
